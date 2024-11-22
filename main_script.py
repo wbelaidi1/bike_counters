45a93f1c4f51554de93a07940004c3c791011251
wbelaidi <william.belaidi@ecl20.ec-lyon.fr>
2024-11-22 22:28:14 +0100

    script V0.1

---
+++ main_script.py
@@ -0,0 +1,91 @@
+## import data
+
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+
+print("hello")
+
+data = pd.read_parquet("data/train.parquet")
+
+def _encode_dates(X):
+    X = X.copy()  # modify a copy of X
+    # Encode the date information from the DateOfDeparture columns
+    X["year"] = X["date"].dt.year
+    X["month"] = X["date"].dt.month
+    X["day"] = X["date"].dt.day
+    X["weekday"] = X["date"].dt.weekday
+    X["hour"] = X["date"].dt.hour
+
+    # Finally we can drop the original columns from the dataframe
+    return X.drop(columns=["date"])
+
+def train_test_split_temporal(X, y, delta_threshold="30 days"):
+    
+    cutoff_date = X["date"].max() - pd.Timedelta(delta_threshold)
+    mask = (X["date"] <= cutoff_date)
+    X_train, X_valid = X.loc[mask], X.loc[~mask]
+    y_train, y_valid = y[mask], y[~mask]
+
+    return X_train, y_train, X_valid, y_valid
+
+def get_train_data(path="data/train.parquet"):
+    data = pd.read_parquet(path)
+    # Sort by date first, so that time based cross-validation would produce correct results
+    data = data.sort_values(["date", "counter_name"])
+    y_array = data[_target_column_name].values
+    X_df = data.drop([_target_column_name, "bike_count"], axis=1)
+    return X_df, y_array
+
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import Ridge
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import OneHotEncoder
+
+X, y = get_train_data()
+X_train, y_train, X_valid, y_valid = train_test_split_temporal(X, y)
+date_encoder = FunctionTransformer(_encode_dates)
+date_cols = _encode_dates(X_train[["date"]]).columns.tolist()
+
+categorical_encoder = OneHotEncoder(handle_unknown="ignore")
+categorical_cols = ["counter_name", "site_name"]
+
+preprocessor = ColumnTransformer(
+    [
+        ("date", OneHotEncoder(handle_unknown="ignore"), date_cols),
+        ("cat", categorical_encoder, categorical_cols),
+    ]
+)
+
+regressor = Ridge()
+
+pipe = make_pipeline(date_encoder, preprocessor, regressor)
+pipe.fit(X_train, y_train)
+
+from sklearn.model_selection import TimeSeriesSplit, cross_val_score
+from sklearn.metrics import mean_squared_error
+
+cv = TimeSeriesSplit(n_splits=6)
+
+# When using a scorer in scikit-learn it always needs to be better when smaller, hence the minus sign.
+scores = cross_val_score(
+    pipe, X_train, y_train, cv=cv, scoring="neg_root_mean_squared_error"
+)
+
+X_test = pd.read_parquet("data/final_test.parquet")
+
+pipe.predict(X_test)
+result = pipe.predict(X_test)
+
+output = pd.DataFrame({
+    'log_bike_count': result
+})
+
+output.index.name = "Id"
+
+output.to_csv('output.csv')
+
+print("result.csv")
